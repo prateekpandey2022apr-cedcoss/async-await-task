942da1949a2deb2108b6b8214b50e17efc7ade8d
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-10-06 14:45:26 +0530

    done

---
+++ src/Home.js
@@ -0,0 +1,361 @@
+import React, { useEffect, useCallback, useState } from "react";
+import {
+  Page,
+  Badge,
+  Card,
+  Select,
+  TextContainer,
+  Button,
+  Loading,
+  Frame,
+  FormLayout,
+  TextField,
+} from "@shopify/polaris";
+import { useFetch } from "./useFetch";
+
+function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [productData, setProductData] = useState({});
+  const [selected, setSelected] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [showAttrs, setShowAttrs] = useState(false);
+  const [attr, setAttr] = useState({});
+  const [attrOptions, setAttrOptions] = useState([]);
+  const [selectedAttrs, setSelectedAttrs] = useState([]);
+  // const [attrOptions, setAttrOptions] = useState([]);
+
+  //  track how many times Add Button is clicked
+  const [attrCounter, setAttrCounter] = useState(0);
+
+  // const [selectArray, setSelectArray] = useState([]);
+
+  // const handleSelectChange = useCallback((value) => setSelected(value), []);
+
+  // let options = [
+  // { label: "Today", value: "today" },
+  // { label: "Yesterday", value: "yesterday" },
+  // { label: "Last 7 days", value: "lastWeek" },
+  // ];
+
+  const { _post } = useFetch(
+    "https://multi-account.sellernext.com/home/public/connector/profile/"
+  );
+
+  useEffect(() => {
+    // debugger;
+    setIsLoading(true);
+    if (showAttrs) {
+      _post("getCategoryAttributes", {
+        target_marketplace: "eyJtYXJrZXRwbGFjZSI6ImFsbCIsInNob3BfaWQiOm51bGx9",
+        data: {
+          barcode_exemption: false,
+          browser_node_id: "1380072031",
+          category: attr.category?.["primary-category"],
+          sub_category: attr.category?.["sub-category"],
+        },
+        user_id: "63329d7f0451c074aa0e15a8",
+        target: {
+          marketplace: "amazon",
+          shopId: "530",
+        },
+        source: {
+          marketplace: "shopify",
+          shopId: "500",
+        },
+      })
+        .then((response) => {
+          // debugger;
+          console.log("get attrs");
+          console.log(response);
+
+          const _attrOptions = [
+            {
+              label: "Select attribute",
+              value: "",
+              disabled: true,
+            },
+          ];
+
+          for (let outer in response.data) {
+            console.log(response.data[outer]);
+            for (let inner in response.data[outer]) {
+              console.log(inner, response.data[outer][inner].label);
+              _attrOptions.push({
+                label: response.data[outer][inner].label,
+                value: inner,
+              });
+            }
+          }
+
+          // debugger;
+
+          setAttrOptions([..._attrOptions]);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, [showAttrs]);
+
+  useEffect(() => {
+    if (selected.length > 0) {
+      setIsLoading(true);
+      _post("getAllCategory", {
+        target_marketplace: "eyJtYXJrZXRwbGFjZSI6ImFsbCIsInNob3BfaWQiOm51bGx9",
+        selected: productData[selected[selected.length - 1]].parent_id,
+        user_id: "63329d7f0451c074aa0e15a8",
+        target: {
+          marketplace: "amazon",
+          shopId: "530",
+        },
+      })
+        .then((response) => {
+          console.log(response);
+
+          if (response.data.length > 0) {
+            const _options = [
+              { label: "Select ...", value: "", disabled: true },
+            ];
+            const _productData = productData;
+
+            response.data.forEach((element) => {
+              _options.push({
+                label: element.name,
+                value: element.browseNodeId,
+                // value: JSON.stringify(element.parent_id),
+                // category: element.category,
+              });
+
+              _productData[element.browseNodeId] = {
+                parent_id: element.parent_id,
+                category: element.category,
+              };
+            });
+            // debugger;
+            // setHasChildren(response.hasChildren);
+            setProductData({ ..._productData });
+            setOptions([...options, _options]);
+          } else {
+            setShowAttrs(true);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, [selected]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    _post("getAllCategory", {
+      target_marketplace: "eyJtYXJrZXRwbGFjZSI6ImFsbCIsInNob3BfaWQiOm51bGx9",
+      selected: [],
+      user_id: "63329d7f0451c074aa0e15a8",
+      target: {
+        marketplace: "amazon",
+        shopId: "530",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        const _options = [{ label: "Select ...", value: "", disabled: true }];
+        const _productData = productData;
+        response.data.forEach((element) => {
+          // debugger;
+          _options.push({
+            label: element.name,
+            value: element.browseNodeId,
+          });
+
+          _productData[element.browseNodeId] = {
+            parent_id: element.parent_id,
+            category: element.category,
+          };
+        });
+        // debugger;
+        setProductData({ ..._productData });
+        setOptions([...options, _options]);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  function createAttrs(idx) {
+    debugger;
+    const optionsCopy = JSON.parse(JSON.stringify(attrOptions));
+
+    // 0
+    // 1 - 0
+    // 2 - 0, 1
+
+    optionsCopy.map((item) => {
+      if (selectedAttrs.includes(item.value)) {
+        item.disabled = true;
+        return item;
+      }
+      return item;
+    });
+
+    // optionsCopy
+    return optionsCopy;
+  }
+
+  return (
+    <Page
+      breadcrumbs={[{ content: "Products", url: "/products" }]}
+      title="3/4 inch Leather pet collar"
+      titleMetadata={<Badge status="success">Paid</Badge>}
+      subtitle="Perfect for any pet"
+      compactTitle
+      primaryAction={{ content: "Save", disabled: true }}
+      secondaryActions={[
+        {
+          content: "Duplicate",
+          accessibilityLabel: "Secondary action label",
+          onAction: () => alert("Duplicate action"),
+        },
+        {
+          content: "View on your store",
+          onAction: () => alert("View on your store action"),
+        },
+      ]}
+      actionGroups={[
+        {
+          title: "Promote",
+          accessibilityLabel: "Action group label",
+          actions: [
+            {
+              content: "Share on Facebook",
+              accessibilityLabel: "Individual action label",
+              onAction: () => alert("Share on Facebook action"),
+            },
+          ],
+        },
+      ]}
+      pagination={{
+        hasPrevious: true,
+        hasNext: true,
+      }}
+    >
+      <>
+        {isLoading && (
+          <div style={{ height: "0px" }}>
+            <Frame>
+              <Loading />
+            </Frame>
+          </div>
+        )}
+
+        <Card title="Add Product " sectioned>
+          <TextContainer>
+            {options.map((item, idx) => {
+              return (
+                <Select
+                  key={idx}
+                  // label="Date range"
+                  options={options[idx]}
+                  onChange={(value) => {
+                    debugger;
+
+                    if (idx < selected.length) {
+                      setShowAttrs(false);
+                      setAttrCounter(0);
+                      setSelectedAttrs([]);
+                    }
+
+                    let temp = selected;
+                    temp = temp.slice(0, idx);
+                    temp[idx] = value;
+                    setAttr({ ...attr, category: productData[value].category });
+                    setSelected([...temp]);
+                    setOptions(options.slice(0, idx + 1));
+                  }}
+                  value={selected?.[idx]}
+                />
+              );
+            })}
+          </TextContainer>
+
+          {showAttrs && (
+            <>
+              <TextContainer>
+                <p>&nbsp;</p>
+                {/* <Heading>Attributes</Heading> */}
+                <Button
+                  primary
+                  onClick={() => setAttrCounter(attrCounter + 1)}
+                  loading={isLoading}
+                >
+                  Add Attribute
+                </Button>
+                {isLoading && <p>Loading Attributes</p>}
+                <FormLayout>
+                  {Array(attrCounter)
+                    .fill(0)
+                    .map((item, idx) => {
+                      return (
+                        <Card sectioned>
+                          <FormLayout.Group>
+                            <Button
+                              plain
+                              textAlign="right"
+                              onClick={(event) => {
+                                debugger;
+                                console.log(event, idx);
+                                selectedAttrs.splice(idx, 1);
+                                setSelectedAttrs([...selectedAttrs]);
+                                setAttrCounter(attrCounter - 1);
+                                // setAttrOptions(attrOptions.slice(idx, idx + 1));
+                                // setSelectedAttrs(selectedAttrs.slice(idx, idx));
+                              }}
+                            >
+                              Delete
+                            </Button>
+                          </FormLayout.Group>
+                          <FormLayout.Group>
+                            {/* <Button>Add product</Button> */}
+                            <Select
+                              key={idx}
+                              label="Select Attribute"
+                              options={createAttrs(idx)}
+                              onChange={(value) => {
+                                // debugger;
+                                const temp = selectedAttrs;
+                                temp[idx] = value;
+                                // setAttr({ ...attr, category: productData[value].category });
+                                setSelectedAttrs([...temp]);
+                                // setAttrOptions([...attrOptions]);
+                              }}
+                              value={selectedAttrs?.[idx]}
+                            />
+                            <TextField
+                              type="text"
+                              label="Input"
+                              onChange={() => {}}
+                              autoComplete="off"
+                            />
+                          </FormLayout.Group>
+                        </Card>
+                      );
+                    })}
+                </FormLayout>
+              </TextContainer>
+            </>
+          )}
+        </Card>
+      </>
+    </Page>
+  );
+}
+
+export default Home;
---
+++ src/useFetch.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+const useFetch = (baseUrl) => {
+  const [url, setUrl] = useState(baseUrl);
+
+  const _get = (endpoint) => {
+    return fetch(baseUrl + endpoint, {
+      method: "GET",
+    }).then((response) => response.json());
+  };
+
+  const _post = (endpoint, payload) => {
+    return fetch(baseUrl + endpoint, {
+      method: "post",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        appTag: "amazon_sales_channel",
+        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiNjI5MGRiYjIzOGUyOWExYjIzMzYwY2E5Iiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNjk2NTY4MDE3LCJpc3MiOiJodHRwczpcL1wvYXBwcy5jZWRjb21tZXJjZS5jb20iLCJ0b2tlbl9pZCI6IjYzM2U1ZjUxYWRkZGFlMjIyNjczN2E5MiJ9.m5LW1XQ_w6E8Y_ZAWV-SqoqLUpgyeQXe3R7aGKhCfkxA0h0i2oESFxS3NXvsqU2zBWO9iPa5vobjXypZCEo7ZbjieaowfryVym-Yc2Kc-SkfHJfr7a2QrXxfKql0nBX0SvgEfVdWKxmVb3AK7MyT60gVUCCh82H7ExXntXA46oTvIQkK2rMTC1pCAFxFcWPTUEvz2yfuyLf62533dDfbdWwnYBxOYXrTUBN9E6aOsbl8MDfglV7bRIiKCXF1hTRjyOzUzqp_Tns4kg3oT2zXKpv7mLFcPpEPnYveRP4TGi_N5gRjfyA4o7xAxTHIxmhlRrY7ZEFUx-BcW6aZz7tYNw`,
+        "Ced-Source-Id": 500,
+        "Ced-Source-Name": "shopify",
+        "Ced-Target-Id": 530,
+        "Ced-Target-Name": "amazon",
+      },
+    }).then((response) => response.json());
+  };
+
+  const _put = (endpoint, payload) => {
+    return fetch(baseUrl + endpoint, {
+      method: "PUT",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then((response) => response.json());
+  };
+
+  const _delete = (endpoint) => {
+    return fetch(baseUrl + endpoint, {
+      method: "DELETE",
+      body: JSON.stringify({}),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then((response) => response.json());
+  };
+
+  // const _request = (method, endpoint, payload) => {
+  //   return fetch(baseUrl + endpoint, {
+  //     method: method,
+  //     body: JSON.stringify(),
+  //   }).then((response) => response.json());
+  // };
+
+  //   const extractDataFromApi = (endpoint, payload, method) => {
+  //     switch (method.toLowerCase()) {
+  //       case "get":
+  //         return _get(endpoint);
+  //       case "post":
+  //         return _post(endpoint, payload);
+  //       case "put":
+  //         return _put(endpoint, payload);
+  //       case "delete":
+  //         return _delete(endpoint);
+  //       default:
+  //         throw new Error(`Illegal method ${method}`);
+  //     }
+  //   };
+
+  //   const _request = ()
+  return { _get, _post, _put, _delete };
+};
+
+export { useFetch };
